6963b9c9fc17e6676b8aca3fde41900d0c4eaeec
Chunxu Yang <chunxuyang@ucla.edu>
2022-10-26 19:32:09 -0700

    feat: complete M2

--- M2/part2/main.js
+++ M2/part2/main.js
@@ -3,7 +3,9 @@
 const noble = require('@abandonware/noble');
 
 const uuid_service = "1101"
-const uuid_value = "2101"
+const uuid_value_x = "2101"
+const uuid_value_y = "2102"
+const uuid_value_z = "2103"
 
 noble.on('stateChange', async (state) => {
   if (state === 'poweredOn') {
@@ -15,20 +17,23 @@ noble.on('stateChange', async (state) => {
 noble.on('discover', async (peripheral) => {
   await noble.stopScanningAsync();
   await peripheral.connectAsync();
-  const {characteristics} = await peripheral.discoverSomeServicesAndCharacteristicsAsync([uuid_service], [uuid_value]);
-  readData(characteristics[0])
+  const services = await peripheral.discoverServicesAsync([uuid_service]);
+  const characteristics = await services[0].discoverCharacteristicsAsync([uuid_value_x, uuid_value_y, uuid_value_z]);
+
+  // read data
+  readData(characteristics);
 });
 
 //
 // read data periodically
 //
-let readData = async (characteristic) => {
-  const value = (await characteristic.readAsync());
-  console.log(value.readFloatLE(0));
+let readData = async (characteristics) => {
+  let x = await characteristics[0].readAsync();
+  let y = await characteristics[1].readAsync();
+  let z = await characteristics[2].readAsync();
 
-  // read data again in t milliseconds
-  setTimeout(() => {
-    readData(characteristic)
-  }, 10);
+  console.log("x: " + x.readFloatLE(0) + ", y: " + y.readFloatLE(0) + ", z: " + z.readFloatLE(0));
+
+  setTimeout(readData, 100, characteristics);
 }
 
